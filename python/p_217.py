666bf6d633b905b7bd0d279392a90b0bde5e6764
Erik Traise <erik.traise@gmail.com>
2025-03-31 09:18:35 +0000

    feat: add sol 217

--- python/p_217.py
+++ python/p_217.py
@@ -1,8 +1,17 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        
 
+        h_map = {} # key : index
 
-nums = [1,2,3,1]
+        for idx, n in enumerate(nums):
+
+            if n in h_map:
+                return True
+            
+            h_map[n] = idx
+
+        return False
+
+nums = [1,2,3,0]
 
 print(Solution().containsDuplicate(nums))
