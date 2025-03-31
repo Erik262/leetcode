c96ca0668aed90d71797ed0b699ea64437760478
Erik Traise <erik.traise@gmail.com>
2025-03-31 10:18:37 +0000

    (wip)

---
+++ python/p_347.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+
+        h_map = {}
+
+        for idx, n in enumerate(nums):
+            
+            if n in h_map:
+                h_map[n] += 1
+            else:
+                h_map[n] = 1
+            
+        
+        h_map = sorted(h_map.items(), key=lambda x: x[1], reverse=True)[:k]
+
+        new_map = []
+        for (a,b) in h_map:
+            new_map.append(a)
+
+        return new_map
+        # return h_map
+
+
+
+# nums = [1,2,2,3,3,3,4]
+nums = [1,9,9,9,3,2,3,4]
+k = 2
+print(Solution().topKFrequent(nums,k))        
