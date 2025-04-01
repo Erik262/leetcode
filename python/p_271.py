799a779c205db5ee878790f4003d11276de23518
Erik Traise <erik.traise@gmail.com>
2025-04-01 07:17:22 +0000

    feat: solve p_271

--- python/p_271.py
+++ python/p_271.py
@@ -1,5 +1,26 @@
 class Solution:
 
     def encode(self, strs: list[str]) -> str:
+        l = ""
+        for w in strs:
+            w = w + ";"
+            l += w
+
+        return l
 
     def decode(self, s: str) -> list[str]:
+        l = []
+        w = ""
+        for c in s:
+            if c == ';':
+                l.append(w)
+                w = ""
+                continue
+            w += c
+
+        return l
+
+strs = ["neet","code","love","you"]
+enc = Solution().encode(strs)
+print(enc)
+print(Solution().decode(enc)) 
