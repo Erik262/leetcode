84ba3642b48e84ef7824a5bebe514134f6e6ec9f
Erik Traise <erik.traise@gmail.com>
2025-04-02 17:40:03 +0000

    feat: solve p238

---
+++ python/p_238.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        
+        arr = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            arr[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in reversed(range(len(nums))):
+            arr[i] *= postfix
+            postfix *= nums[i]
+
+        return arr
+
+nums = [1,2,3,4]
+nums2 = [-1,1,0,-3,3]
+
+print(Solution().productExceptSelf(nums))
