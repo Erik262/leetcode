eaae07b9924c071976e9858c75444cee35dd18eb
Erik Traise <erik.traise@gmail.com>
2025-04-04 10:57:37 +0000

    chore: ruff formatting

--- python/p_128.py
+++ python/p_128.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution:
     def longestConsecutive(self, nums):
         counter = 0
         best = []
@@ -6,7 +6,7 @@ class Solution(object):
         if len(nums) == 0:
             best.append(counter)
             return counter
-        
+
         s_nums = sorted(nums)
         for idx, n in enumerate(s_nums):
             if idx == 0:
@@ -14,22 +14,17 @@ class Solution(object):
                 best.append(counter)
                 continue
             diff = abs(n - s_nums[idx - 1])
-            if (diff == 1):
+            if diff == 1:
                 counter += 1
                 best.append(counter)
 
             if diff > 1:
                 counter = 1
-        
+
         return max(best)
-        
 
 
-
-
-
-
-nums = [2,20,4,10,3,4,5] # 4
+nums = [2, 20, 4, 10, 3, 4, 5]  # 4
 # nums = [0,-1] # 2
 # nums = [9,1,4,7,3,-1,0,5,8,-1,6] # 7
 # nums = [9,1,4,7,3,-1,0,5,8,-1,6] # 7
