93f20631fdf375b5c402df0917c1c53717f54679
Erik Traise <erik.traise@gmail.com>
2025-04-06 12:05:42 +0000

    feat: finalize solution of p_15

--- python/p_15.py
+++ python/p_15.py
@@ -1,69 +1,36 @@
 class Solution(object):
     def threeSum(self, nums):
-
-        l_idx = 0
-        m_idx = l_idx + 1
-        r_idx = m_idx + 1
+        nums.sort()
         result = []
 
-        while l_idx < m_idx:
+        for l_idx in range(len(nums) - 2):
             l_value = nums[l_idx]
-            m_value = nums[m_idx]
-            r_value = nums[r_idx]
+            if l_idx > 0 and l_value == nums[l_idx - 1]:
+                continue
+            m_idx = l_idx + 1
+            r_idx = len(nums) - 1
 
-            target = l_value + m_value + r_value
-            print(target)
+            while m_idx < r_idx:
+                diff = nums[l_idx] + nums[m_idx] + nums[r_idx] 
 
-            if target == 0:
-                result.append([l_value, m_value, r_value])
+                if diff == 0:
+                    result.append([l_value, nums[m_idx], nums[r_idx]])
 
-            
+                    while m_idx < r_idx and nums[m_idx] == nums[m_idx + 1]:
+                        m_idx += 1
 
+                    while m_idx < r_idx and nums[r_idx] == nums[r_idx - 1]:
+                        r_idx -= 1
 
+                    r_idx -= 1
+                    m_idx += 1
+                
+                elif diff < 0:
+                    m_idx += 1
+                else:
+                    r_idx -= 1                
+                    
         return result
-
-
-
-        # s_nums = sorted(nums)
-
-        # l_idx = 0
-        # r_idx = len(s_nums) - 1
-        # m_idx = l_idx + 1
-        # result = []
-
-        # while l_idx < r_idx:
-        #     l_value = s_nums[l_idx]
-        #     r_value = s_nums[r_idx]
-        #     m_value = s_nums[m_idx]
-
-        #     target = l_value + m_value + r_value
-
-        #     if target == 0:
-        #         if [l_value, m_value, r_value] not in result:
-        #             result.append([l_value, m_value, r_value])
-
-        #     while m_idx < r_idx:
-        #         target = l_value + s_nums[m_idx] + r_value
-
-        #         if target == 0:
-        #             if [l_value, s_nums[m_idx], r_value] not in result:
-        #                 result.append([l_value, s_nums[m_idx], r_value])
-                
-        #         m_idx += 1
-                
-        #     if target < 0:
-        #         l_idx += 1
-        #     else:
-        #         r_idx -=1
-            
-        #     m_idx = l_idx + 1
-            
-        # return result
-
-
-
-
-
 
 nums = [-1,0,1,2,-1,-4] # [[-1,-1,2],[-1,0,1]]
 
@@ -72,5 +39,6 @@ nums = [-1,0,1,2,-1,-4] # [[-1,-1,2],[-1,0,1]]
 # nums = [0,0,0] # [[0,0,0]]
 
 # nums=[-1,0,1,0] # [[-1,0,1]]
+# nums=[0,0,0,0] # [[0,0,0]]
 
 print(Solution().threeSum(nums))
