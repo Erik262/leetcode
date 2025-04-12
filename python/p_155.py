a140a4d7d7ef6053b2c0aaf9c71c523587e7f63e
Erik Traise <erik.traise@gmail.com>
2025-04-12 14:06:31 +0200

    add solution for p_155

---
+++ python/p_155.py
@@ -0,0 +1,47 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if len(self.min_stack) == 0 or self.min_stack[-1] >= val:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.min_stack[-1] == self.stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+obj.getMin()
+obj.pop()
+obj.top()
+obj.getMin()
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+
+# Output
+# [null,null,null,null,-3,null,0,-2]
