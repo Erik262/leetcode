a2e01103a5aff0ccf0e52214b5d3cf60dd205548
Erik Traise <erik.traise@gmail.com>
2025-04-28 22:41:19 +0200

    feat: implement solution for Invert Binary Tree problem

---
+++ python/226.invert-binary-tree.py
@@ -0,0 +1,74 @@
+from typing import Optional
+# @lc app=leetcode id=226 lang=python3
+#
+# [226] Invert Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root_left = root.left
+        root_right = root.right
+
+        root.left = root_right
+        root.right = root_left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+
+def list_to_tree(data: list[Optional[int]]) -> Optional[TreeNode]:
+    if not data:
+        return None
+    nodes = [TreeNode(val) if val is not None else None for val in data]
+    kid_index = 1
+    for node in nodes:
+        if node is not None:
+            if kid_index < len(nodes):
+                node.left = nodes[kid_index]
+                kid_index += 1
+            if kid_index < len(nodes):
+                node.right = nodes[kid_index]
+                kid_index += 1
+    return nodes[0]
+
+
+def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
+    if root is None:
+        return []
+    result: list[Optional[int]] = []
+    queue: list[Optional[TreeNode]] = [root]
+    while queue:
+        node = queue.pop(0)
+        if node is not None:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
+
+
+root = [4,2,7,1,3,6,9] # [4,7,2,9,6,3,1]
+# root = [2,1,3] # [2,3,1]
+# root = [] # []
+res = Solution().invertTree(list_to_tree(root))
+print(tree_to_list(res))
+
+        
+# @lc code=end
+
