e81c3ca6fd60e76bdfb79378731ade0cd4d1fd68
Erik Traise <erik.traise@gmail.com>
2025-05-06 23:20:12 +0200

    feat: add solution for Binary Tree Right Side View problem

---
+++ python/199.binary-tree-right-side-view.py
@@ -0,0 +1,81 @@
+from typing import Optional, List
+from collections import deque
+# @lc app=leetcode id=199 lang=python3
+#
+# [199] Binary Tree Right Side View
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        result = []
+
+        while queue:
+            level_size = len(queue)
+
+            for i in range(level_size):
+                node = queue.popleft()
+
+                if i == level_size - 1:
+                    result.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+        return result
+            
+
+def list_to_tree(data: list[Optional[int]]) -> Optional[TreeNode]:
+    if not data:
+        return None
+    nodes = [TreeNode(val) if val is not None else None for val in data]
+    kid_index = 1
+    for node in nodes:
+        if node is not None:
+            if kid_index < len(nodes):
+                node.left = nodes[kid_index]
+                kid_index += 1
+            if kid_index < len(nodes):
+                node.right = nodes[kid_index]
+                kid_index += 1
+    return nodes[0]
+
+
+def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
+    if root is None:
+        return []
+    result: list[Optional[int]] = []
+    queue: list[Optional[TreeNode]] = [root]
+    while queue:
+        node = queue.pop(0)
+        if node is not None:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
+
+root = [1,2,3] # [1,3]
+# root = [1,2,3,4,5,6,7] # [1,3,7]
+
+print(Solution().rightSideView(list_to_tree(root)))
+        
+# @lc code=end
+
