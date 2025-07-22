dfff2f31c01b19e14d06ff58bb0aa4fdb7d65eed
Erik Traise <erik.traise@gmail.com>
2025-07-22 13:12:57 +0200

    feat: implement Longest Common Prefix solution to find the longest common prefix string among an array of strings

---
+++ python/14.longest-common-prefix.py
@@ -0,0 +1,28 @@
+from typing import List
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != char:
+                    return strs[0][:i]
+                
+        return strs[0]
+
+
+strs = ["flower","flow","flight"]
+# strs = ["dog","racecar","car"]
+
+print(Solution().longestCommonPrefix(strs))
+        
+# @lc code=end
+
