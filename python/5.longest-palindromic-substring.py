6abad3fcdc0b6264f376e337384d540b2557948b
Erik Traise <erik.traise@gmail.com>
2025-09-07 19:09:08 +0200

    feat: revisit problem 5

--- python/5.longest-palindromic-substring.py
+++ python/5.longest-palindromic-substring.py
@@ -7,29 +7,25 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        result = ""
-        result_length = 0
+        max_pali = ""
+
+        def expand(s, l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+
+            return s[l+1:r]
 
         for i in range(len(s)):
+            l1 = expand(s, i, i) # odd
+            l2 = expand(s, i, i + 1) # even
 
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > result_length:
-                    result = s[l: r + 1]
-                    result_length = r - l + 1
-                l -= 1
-                r += 1
+            longest = l1 if len(l1) > len(l2) else l2
 
+            if len(longest) > len(max_pali):
+                max_pali = longest
 
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > result_length:
-                    result = s[l: r + 1]
-                    result_length = r - l + 1
-                l -= 1
-                r += 1
-
-        return result
+        return max_pali
                  
 
 
