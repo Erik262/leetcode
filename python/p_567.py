8915c8865a8a38ab4fa8259393a61e1c57195d87
Erik Traise <erik.traise@gmail.com>
2025-10-01 11:54:18 +0200

    feat: improve old solution

--- python/p_567.py
+++ python/p_567.py
@@ -1,58 +1,18 @@
+from collections import Counter
 class Solution(object):
     def checkInclusion(self, s1, s2):
-        if len(s1) > len(s2):
-            return False
-        
-        s1_counter = [0] * 26
-        s2_counter = [0] * 26
-        matches = 0
+        freq_s1 = Counter(s1)
+
         l = 0
 
-        for i in range(len(s1)): # a = 97, b = 98
-            # to become 'a' indexed as 0, 'b' as 1, 'c' as 2 etc...
-            s1_counter[ord(s1[i]) - ord("a")] += 1 
-            s2_counter[ord(s2[i]) - ord("a")] += 1
-
-        for i in range(26):
-            if s1_counter[i] == s2_counter[i]:
-                matches += 1
-
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
+        for r in range(len(s1), len(s2) + 1):
+            freq_s2 = Counter(s2[l:r])
+            if freq_s1 == freq_s2:
                 return True
-            
-            idx = ord(s2[r]) - ord('a')
-            s2_counter[idx] += 1 # next character in our sliding window
-
-            if s1_counter[idx] == s2_counter[idx]:
-                matches += 1
-
-            elif s1_counter[idx] + 1 == s2_counter[idx]:
-                matches -= 1
-
-            idx = ord(s2[l]) - ord('a')
-            s2_counter[idx] -= 1
-
-            if s1_counter[idx] == s2_counter[idx]:
-                matches += 1
-
-            elif s1_counter[idx] - 1 == s2_counter[idx]:
-                matches -= 1
 
             l += 1
 
-        return matches == 26
-
-
-        
-
-
-
-
-
-
-
-
+        return False
 
 # s1 = "aab"
 # s2 = "aabaabaab" # True
