bcef726643854daf10999063ac13538454a1f883
Erik Traise <erik.traise@gmail.com>
2025-10-09 09:58:30 +0200

    feat: update my solution

--- python/23.merge-k-sorted-lists.py
+++ python/23.merge-k-sorted-lists.py
@@ -12,41 +12,43 @@ class ListNode:
         self.next = next
         
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
-            return None
-
-        for i in range(1, len(lists)):
-            lists[i] = self._merge(lists[i - 1], lists[i])
-        
-        return lists[-1]
-
-    def _merge(self, l1, l2):
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1, l2):
         head = ListNode()
-        tail = head
-
+        curr = head
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                curr.next = l1
                 l1 = l1.next
-                tail = tail.next
-
-            elif l1.val > l2.val:
-                tail.next = l2
-                l2 = l2.next
-                tail = tail.next
             else:
-                tail.next = l1
-                l1 = l1.next
-                tail = tail.next
-
-        # If one list becomes empty
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
         if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
-
+            curr.next = l1
+        elif l2:
+            curr.next = l2
+            
         return head.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        
+        if len(lists) == 1:
+            return lists[0]
+            
+        mid = len(lists) // 2
+        left_merged = self.mergeKLists(lists[:mid])
+        right_merged = self.mergeKLists(lists[mid:])
+        
+        return self.mergeTwoLists(left_merged, right_merged)
 
 def list_to_linkedlist(items: List[int]) -> Optional[ListNode]:
     dummy = ListNode()
