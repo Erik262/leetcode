84440312ea2436320cf57b2091619aa0c4f32879
Erik Traise <erik.traise@gmail.com>
2025-10-28 14:37:53 +0100

    feat: nicer looking solution

--- python/13.roman-to-integer.py
+++ python/13.roman-to-integer.py
@@ -7,30 +7,27 @@
 # @lc code=start
 class Solution:
     def romanToInt(self, s: str) -> int:
-        amount = 0
-        hmap = {
-            "M": 1000,
-            "D": 500,
-            "C": 100,
-            "L": 50,
-            "X": 10,
-            "V": 5,
+        roman = {
             "I": 1,
-            "0": 0
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
+        result = 0
 
-        prev = hmap["0"]
-        for c in s:
-            diff = 0
-            val = hmap[c]
-            if prev < val:
-                diff = val - 2 * prev
-                amount += diff
+        for i in range(len(s) - 1):
+            pre = roman[s[i]]
+            pos = roman[s[i+1]]
+
+            if pre < pos:
+                result -= pre
             else:
-                amount += val
-            prev = hmap[c]
-            
-        return amount
+                result += pre
+
+        return result + roman[s[-1]]
 
 s = "III" # 3
 s = "LVIII" # 58
