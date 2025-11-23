444fe1025e84a11b01d958638f937892b6db4e1f
Erik Traise <erik.traise@gmail.com>
2025-11-23 08:53:41 +0100

    feat: slightly some improvements

--- python/40.combination-sum-ii.py
+++ python/40.combination-sum-ii.py
@@ -7,28 +7,30 @@ from typing import List
 # @lc code=start
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        tmp_val = []
+        result = []
         candidates.sort()
 
-        def dfs(idx: int, tmp_val: List[int], total: int):
-            if total == target:
-                res.append(tmp_val.copy())
+        def dfs(idx: int, tmp_list: List[int], tot_val: int):
+            if tot_val == target:
+                result.append(tmp_list.copy())
                 return
             
-            if total > target or idx >= len(candidates):
+            if tot_val > target:
                 return
-            
+
+            if idx >= len(candidates) or tot_val > target:
+                return
+
             for i in range(idx, len(candidates)):
-                if i > idx and candidates[i] == candidates[i - 1]:
+                if i > idx and candidates[i] == candidates[i-1]:
                     continue
 
-                tmp_val.append(candidates[i])
-                dfs(i + 1, tmp_val, total + candidates[i])
-                tmp_val.pop()
+                tmp_list.append(candidates[i])
+                dfs(i + 1, tmp_list, tot_val + candidates[i])
+                tmp_list.pop()
 
-        dfs(0, tmp_val, 0)
-        return res
+        dfs(0, [], 0)
+        return result 
 
 
 candidates = [9,2,2,4,6,1,5] # [[1,2,5], [2,2,4], [2,6]]
