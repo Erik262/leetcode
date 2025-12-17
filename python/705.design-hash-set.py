7706b4d8c1244a388966b929362bfd74ebee7f33
Erik Traise <erik.traise@gmail.com>
2025-12-17 09:47:15 +0100

    feat: implement MyHashSet class

---
+++ python/705.design-hash-set.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=705 lang=python3
+#
+# [705] Design HashSet
+#
+
+# @lc code=start
+class MyHashSet:
+
+    def __init__(self):
+        self.hash = []
+
+    def add(self, key: int) -> None:
+        if key not in self.hash:
+            self.hash.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.hash:
+            self.hash.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.hash
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
+# @lc code=end
+
