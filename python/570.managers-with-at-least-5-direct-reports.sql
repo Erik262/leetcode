2c232c895a1a09ad4bc5611a17587303962d9a98
Erik Traise <erik.traise@gmail.com>
2026-01-27 09:24:35 +0100

    feat: add MySQL solution for Managers with at Least 5 Direct Reports problem

---
+++ python/570.managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=570 lang=mysql
+--
+-- [570] Managers with at Least 5 Direct Reports
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT e1.name 
+FROM Employee e1
+JOIN Employee e2
+ON e1.id = e2.managerID 
+GROUP BY e2.managerID
+HAVING COUNT(*) >= 5;
+-- @lc code=end
+
