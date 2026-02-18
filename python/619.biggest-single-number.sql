0ba58f7048da706abd375db46051765a14218207
Erik Traise <erik.traise@gmail.com>
2026-02-18 17:05:34 +0100

    feat: add MySQL solution for Biggest Single Number problem

---
+++ python/619.biggest-single-number.sql
@@ -0,0 +1,17 @@
+--
+-- @lc app=leetcode id=619 lang=mysql
+--
+-- [619] Biggest Single Number
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM mynumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS t
+-- @lc code=end
+
